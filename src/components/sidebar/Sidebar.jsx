5e1d0a7d58e07f3269d89d3e38e6ceac9fd2fe98
Michael Chilaka <obichilaka@gmail.com>
2022-08-03 16:29:34 +0100

    updates sidebar component

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -14,6 +14,23 @@ export default function Sidebar() {
         <div className="sidebar__menu">
           <h5 className="sidebar__title">Main Menu</h5>
           <ul className="sidebar__list">
+            <li className="sidebar__listItem active">
+              <BsFillGridFill /> Dashboard
+            </li>
+            <li className="sidebar__listItem">
+              <MdPanoramaPhotosphereSelect /> Catering Status
+            </li>
+            <li className="sidebar__listItem">
+              <RiBarChartBoxFill /> Guest traffic
+            </li>
+            <li className="sidebar__listItem">
+              <IoGiftSharp /> Gifts
+            </li>
+          </ul>
+        </div>
+        <div className="sidebar__menu">
+          <h5 className="sidebar__title">Main Menu</h5>
+          <ul className="sidebar__list">
             <li className="sidebar__listItem">
               <BsFillGridFill /> Dashboard
             </li>
