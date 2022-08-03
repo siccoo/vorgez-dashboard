25a9ddba3f748248e56ec596825adb21162c49a4
Michael Chilaka <obichilaka@gmail.com>
2022-08-03 14:50:28 +0100

    updates on topbar component

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,15 +1,31 @@
-import React from 'react';
+import React from "react";
 import "./topbar.css";
+import { IoIosNotifications } from "react-icons/io";
+
+import VorgezLogo from "../../assets/images/vorgez-logo.png";
 
 const Topbar = () => {
   return (
-    <div className='topbar'>
-        <div className='topbar__wrapper'>
-            <div className='topbar__left'>Left</div>
-            <div className='topbar__right'>Right</div>
+    <div className="topbar">
+      <div className="topbar__wrapper">
+        <div className="topbar__left">
+          <img
+            className="vorgez-logo"
+            src={VorgezLogo}
+            alt="vorgez-logo"
+            loading="lazy"
+            width="100"
+          />
         </div>
+        <div className="topbar__right">
+          <div className="icons__container">
+            <IoIosNotifications className="notify" />
+            <span className="badge">3</span>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Topbar
+export default Topbar;
