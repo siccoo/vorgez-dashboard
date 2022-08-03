588b67943b2f4625f7035571a4c7163282236020
Michael Chilaka <obichilaka@gmail.com>
2022-08-03 23:43:32 +0100

    updates on GuestList table

--- src/pages/guestList/GuestList.jsx
+++ src/pages/guestList/GuestList.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { DataGrid } from "@material-ui/data-grid";
 
 import "./guestList.css";
@@ -6,29 +5,29 @@ import "./guestList.css";
 export default function GuestList() {
 
     const columns = [
-        { field: 'id', headerName: 'ID', width: 90 },
+        { field: 'id', headerName: 'ID', width: 110 },
         {
-          field: 'firstName',
-          headerName: 'First name',
+          field: 'name',
+          headerName: 'Name',
           width: 150,
+        //   editable: true,
+        },
+        {
+          field: 'contact',
+          headerName: 'Contact',
+          width: 160,
           editable: true,
         },
         {
-          field: 'lastName',
-          headerName: 'Last name',
-          width: 120,
-          editable: true,
-        },
-        {
-          field: 'age',
-          headerName: 'Age',
+          field: 'status',
+          headerName: 'Status',
           type: 'number',
-          width: 110,
+          width: 160,
           editable: true,
         },
         {
-          field: 'fullName',
-          headerName: 'Full name',
+          field: 'address',
+          headerName: 'Address',
           description: 'This column has a value getter and is not sortable.',
           sortable: false,
           width: 160,
@@ -36,23 +35,64 @@ export default function GuestList() {
       ];
       
       const rows = [
-        { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-        { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-        { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-        { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-        { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-        { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-        { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-        { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-        { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+        {   id: 1, 
+            name: 'Snow', 
+            avatar: 'https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500', 
+            contact: 'snow@gmail.com, +123456789',
+            status: 'come in',
+            address: 'Lagos, Nigeria'
+        },
+        {   id: 2, 
+            name: 'Snow', 
+            avatar: 'https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500', 
+            contact: 'snow@gmail.com, +123456789',
+            status: 'come in',
+            address: 'Lagos, Nigeria'
+        },
+        {   id: 3, 
+            name: 'Snow', 
+            avatar: 'https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500', 
+            contact: 'snow@gmail.com, +123456789',
+            status: 'come in',
+            address: 'Lagos, Nigeria'
+        },
+        {   id: 4, 
+            name: 'Snow', 
+            avatar: 'https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500', 
+            contact: 'snow@gmail.com, +123456789',
+            status: 'come in',
+            address: 'Lagos, Nigeria'
+        },
+        {   id: 5, 
+            name: 'Snow', 
+            avatar: 'https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500', 
+            contact: 'snow@gmail.com, +123456789',
+            status: 'come in',
+            address: 'Lagos, Nigeria'
+        },
+        {   id: 6, 
+            name: 'Snow', 
+            avatar: 'https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500', 
+            contact: 'snow@gmail.com, +123456789',
+            status: 'come in',
+            address: 'Lagos, Nigeria'
+        },
+        {   id: 7 , 
+            name: 'Snow', 
+            avatar: 'https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500', 
+            contact: 'snow@gmail.com, +123456789',
+            status: 'come in',
+            address: 'Lagos, Nigeria'
+        },
+        
       ];
 
   return (
-    <div className="guestList">
+    <div className="guestList" style={{ height: 500, width: '100%' }}>
       <DataGrid
         rows={rows}
         columns={columns}
-        pageSize={8}
+        pageSize={7}
         checkboxSelection
         disableSelectionOnClick
       />
