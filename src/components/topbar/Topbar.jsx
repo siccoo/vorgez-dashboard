e684ad86d7919979860fd26a03b0b1de74baa4f9
Michael Chilaka <obichilaka@gmail.com>
2022-08-03 13:03:06 +0100

    updates on topbar component

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./topbar.css";
+
+const Topbar = () => {
+  return (
+    <div className='topbar'>
+        <div className='topbar__wrapper'>
+            <div className='topbar__left'>Left</div>
+            <div className='topbar__right'>Right</div>
+        </div>
+    </div>
+  )
+}
+
+export default Topbar
