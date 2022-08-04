89184c983bee7e68bc974a9db4d296bb9f5aa576
Michael Chilaka <obichilaka@gmail.com>
2022-08-04 13:41:53 +0100

    updates on Guest components

--- src/pages/guest/Guest.jsx
+++ src/pages/guest/Guest.jsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React from "react";
 import "./guest.css";
 
 export default function Guest() {
   return (
-    <div className='guest'>
-        <div className="guestHeadContainer">
-            <h2 className="guestHead">Edit Guest</h2>
-            <button className="guestAddBtn">Create</button>
-        </div>
+    <div className="guest">
+      <div className="guestHeadContainer">
+        <h2 className="guestHead">Edit Guest</h2>
+        <button className="guestAddBtn">Create</button>
+      </div>
+      <div className="guestContainer">
+        <div className="guestShow"></div>
+        <div className="guestUpdate"></div>
+      </div>
     </div>
-  )
+  );
 }
